c3140ef302f49597ff151ed346c528034eb8961a
GenCEO <151505484+GenCEO@users.noreply.github.com>
2021-09-05 10:55:54 +0000

    Updated mote

--- main.go
+++ main.go
@@ -9,3 +9,9 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	dialogflow "cloud.google.com/go/dialogflow/apiv2"
+	"github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/api/option"
+	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+)
