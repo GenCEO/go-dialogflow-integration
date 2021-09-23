f0a47523703b4c07cc3b0931556c912288d8e276
GenCEO <151505484+GenCEO@users.noreply.github.com>
2021-09-23 06:43:21 +0000

    Flesh out the README

--- main.go
+++ main.go
@@ -29,3 +29,8 @@ type DialogflowProcessor struct {
 // NLPResponse is the struct for the response
 type NLPResponse struct {
 	Intent     string            `json:"intent"`
+	Confidence float32           `json:"confidence"`
+	Entities   map[string]string `json:"entities"`
+}
+
+var dp DialogflowProcessor
