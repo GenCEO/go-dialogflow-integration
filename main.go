cd372f946b955e912cb9d46d60e314e40976ea5d
GenCEO <151505484+GenCEO@users.noreply.github.com>
2021-10-30 14:47:12 +0000

    cleaned up config

--- main.go
+++ main.go
@@ -63,3 +63,5 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		response := dp.processNLP(m.Message, "testUser")
 		fmt.Printf("%#v", response)
 		w.Header().Set("Content-Type", "application/json")
+		//json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(response)
