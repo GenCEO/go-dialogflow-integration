d588ad8adb521387098af95335504304be749d4d
GenCEO <151505484+GenCEO@users.noreply.github.com>
2021-10-30 21:38:04 +0000

    edit about

--- main.go
+++ main.go
@@ -65,3 +65,4 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		//json.NewEncoder(w).Encode(response)
 		json.NewEncoder(w).Encode(response)
+	}
